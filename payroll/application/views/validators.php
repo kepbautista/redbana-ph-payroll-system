292caef20ba22004cc9c10df6ba14a16c61b5144
kepbautista@gmail.com <kepbautista@gmail.com@65eff120-3760-e550-e617-1429e56cfc85>
2011-05-02 16:29:31 +0000

    includes form validation using ajax and jquery

---
+++ payroll/application/views/validators.php
@@ -0,0 +1,56 @@
+<script type="text/javascript" src="<?php echo base_url();?>devtools/jquery-1.5.2.js"></script>
+<script type="text/javascript">
+	i=j=k=l=0;
+	$(document).ready(function(){
+		$('#username').blur(function(){
+			$.post("<?php echo base_url();?>devtools/validate.php", {
+             query: $('#username').val(),
+			 vtype: "uname",
+          },
+          function(data){
+                $("#uname").text(data);
+          }
+		  );
+		});
+		$('#password').blur(function(){
+			$.post("<?php echo base_url();?>devtools/validate.php", {
+             query: $('#password').val(),
+			 vtype: "password",
+          },
+          function(data){
+                $("#pword").text(data);
+          }
+		  );
+		});
+		$('#passconf').blur(function(){
+			$.post("<?php echo base_url();?>devtools/validate.php", {
+             query: $('#passconf').val(),
+			 vtype: "password",
+          },
+          function(data){
+                $("#pconf").text(data);
+          }
+		  );
+		});
+		$('#email').blur(function(){
+			$.post("<?php echo base_url();?>devtools/validate.php", {
+             query: $('#email').val(),
+			 vtype: "e-mail",
+          },
+          function(data){
+                $("#eMail").text(data);
+          }
+		  );
+		});
+		$('.numeric').blur(function(){
+			$.post("<?php echo base_url();?>devtools/validate.php", {
+             query: $(this).val(),
+			 vtype: "numeric",
+          },
+          function(data){
+			if(data!='') alert(data);
+          }
+		  );
+		});
+	});
+	</script>
