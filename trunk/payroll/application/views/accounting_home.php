240bd99e1d9ecc4006bb02f5f75bb1127204b0cf
mrbgarra <mrbgarra@65eff120-3760-e550-e617-1429e56cfc85>
2011-05-25 02:24:41 +0000

    checking of some coding standards...

--- trunk/payroll/application/views/accounting_home.php
+++ trunk/payroll/application/views/accounting_home.php
@@ -1,32 +1,28 @@
-<html>
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head> 
-	<title> REDBANA PAYROLL SYSTEM</title>
+	<title> REDBANA PAYROLL SYSTEM </title>
 </head>
+
 <body>
-
-
-		<br /><br /><br />
-		
-		<div id="drdw">
-<div>
-Welcome accounting officer! :)
+<br /><br /><br />	
+<div id="drdw">
+<div> Welcome accounting officer! :)
 <?php 
-	echo "<a class = 'active' href = 'login/logout' id = 'cname' alignment = 'left' > Sign out </a>";				
+	echo "<a class='active' href='login/logout' id='cname' alignment='left' > Sign out </a>";				
 ?>	
 			
-    <ul>    	
-    	<li><a href="#" class="underline">Edit payroll</a></li>
-		<li><a href="#" class="underline">View payroll</a></li>
-		<li><a href="sss/getall" class="underline">Edit SSS table</a></li>
-		<li><a href="#" class="underline">Edit PhilHealth table</a></li>
-		<li><a href="witholdingtaxcontroller" class="underline">Edit Payment modes</a></li>
+	<ul>    	
+    	<li><a href="#" class="underline"> Edit payroll </a></li>
+		<li><a href="#" class="underline"> View payroll </a></li>
+		<li><a href="sss/getall" class="underline"> Edit SSS table </a></li>
+		<li><a href="#" class="underline"> Edit PhilHealth table </a></li>
+		<li><a href="witholdingtaxcontroller" class="underline"> Edit Payment modes </a></li>
     </ul>
-
 </div>
-
-
 </div>
 <br /><br />
- 
 </body>
 </html>
--- trunk/payroll/application/views/changeWitholdingTaxSuccess-temp.php
+++ trunk/payroll/application/views/changeWitholdingTaxSuccess-temp.php
@@ -1,12 +1,16 @@
-<html>
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<title>My Form</title>
+	<title> Edit Successful - Redbana Phils. Payroll </title>
 </head>
+
 <body>
-
-<h3>Your form was successfully submitted!</h3>
-
+<h3> Your form was successfully submitted! </h3>
 <p><?php echo anchor('form', 'Try it again!'); ?></p>
-
+<?php 
+	echo "<a class='active' href='login/logout' id='cname' alignment='left' > Sign out </a>";				
+?>
 </body>
 </html>
