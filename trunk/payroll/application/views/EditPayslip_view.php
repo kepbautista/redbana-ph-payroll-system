2390cdb22c4f651feee4fd156e2eb26d45963044
kepbautista@gmail.com <kepbautista@gmail.com@65eff120-3760-e550-e617-1429e56cfc85>
2011-05-30 09:35:47 +0000

    Ajax & Jquery validation for EditPayslip_view.php

--- trunk/payroll/application/views/EditPayslip_view.php
+++ trunk/payroll/application/views/EditPayslip_view.php
@@ -8,11 +8,119 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
 <head>
 	<title>Edit Pay Slip</title>
+	<script type="text/javascript" src="<?php echo base_url();?>devtools/jquery-1.5.2"></script>
+	<script>
+		$(document).ready(function(){
+			$('#DailyRate').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#DailyRate').val(),
+				vtype: "numeric",
+			},
+			function(data){
+					$("#daily_rate").text(data);
+			});
+			});
+			$('#HolidayAdjustment').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#HolidayAdjustment').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#holiday_adj").text(data);
+			});
+			});
+			$('#TaxRefund').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#TaxRefund').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#tax_ref").text(data);
+			});
+			});
+			$('#NonTax').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#NonTax').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#non_tax").text(data);
+			});
+			});
+			$('#TaxShield').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#TaxShield').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#tax_shield").text(data);
+			});
+			});
+			$('#PagibigLoan').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#PagibigLoan').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#pagibig_loan").text(data);
+			});
+			});
+			$('#SSSLoan').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#SSSLoan').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#sss_loan").text(data);
+			});
+			});
+			$('#CompanyLoan').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#CompanyLoan').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#comp_loan").text(data);
+			});
+			});
+			$('#CellphoneCharges').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#CellphoneCharges').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#cellphone").text(data);
+			});
+			});
+			$('#AdvancestoEmployee').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#AdvancestoEmployee').val(),
+				vtype: "salary",
+			},
+			function(data){
+					$("#advances").text(data);
+			});
+			});
+			$('#Status').blur(function(){
+				$.post("<?php echo base_url();?>devtools/validate.php", {
+				query: $('#Status').val(),
+				vtype: "open",
+			},
+			function(data){
+					$("#stat").text(data);
+			});
+			});
+		});
+	</script>
 	<style>
 		th{text-align: left;}
 		.numeric{text-align: right; width: 60px}
 		td{text-align: right;}
 	</style>
+	<?php
+		include 'links.php';
+		include 'styles.php';
+	?>
 </head>
 
 <body>
@@ -40,6 +148,7 @@
 		<th>Daily Rate:</th>
 		<td>
 		<input type='text' class='numeric' id='DailyRate' name='DailyRate' value='<?php echo $DailyRate;?>'/>
+		</td><td style='text-align:left'><span class='warning' name='daily_rate' id='daily_rate'></span>
 		</td>
 	</tr>
 	<tr>
@@ -72,10 +181,14 @@
 	</tr>
 	<tr>
 		<th>Holiday Adjustment:</th>
-		<td><input type='text' class='numeric' id='HolidayAdjustment' name='HolidayAdjustment' value='<?php echo $HolidayAdjustment;?>'/></td>
+		<td><input type='text' class='numeric' id='HolidayAdjustment' name='HolidayAdjustment' value='<?php echo $HolidayAdjustment;?>'/>
+		</td><td style='text-align:left'><span class='warning' name='holiday_adj' id='holiday_adj'></span>
+		</td>
 	<tr>
 		<th>Salary Adjustment (Tax Refund):</th>
-		<td><input type='text' class='numeric' id='TaxRefund' name='TaxRefund' value='<?php echo $TaxRefund;?>'/></td>
+		<td><input type='text' class='numeric' id='TaxRefund' name='TaxRefund' value='<?php echo $TaxRefund;?>'/>
+		</td><td style='text-align:left'><span class='warning' name='tax_ref' id='tax_ref'></span>
+		</td>
 	</tr>
 	<tr>
 		<th>Night Differential:</th>
@@ -93,11 +206,15 @@
 	</tr>
 	<tr>
 		<th>Non-Tax:</th>
-		<td><input type='text' class='numeric' id='NonTax' name='NonTax' value='<?php echo $NonTax;?>'></td>
+		<td><input type='text' class='numeric' id='NonTax' name='NonTax' value='<?php echo $NonTax;?>'>
+		</td><td style='text-align:left'><span class='warning' name='non_tax' id='non_tax'></span>
+		</td>
 	</tr>
 	<tr>
 		<th>Tax Shield:</th>
-		<td><input type='text' class='numeric' id='TaxShield' name='TaxShield' value='<?php echo $TaxShield;?>'></td>
+		<td><input type='text' class='numeric' id='TaxShield' name='TaxShield' value='<?php echo $TaxShield;?>'>
+		</td><td style='text-align:left'><span class='warning' name='tax_shield' id='tax_shield'></span>
+		</td>
 	</tr>
 	<tr>
 		<th>Total Pay:</th>
@@ -143,23 +260,33 @@
 	</tr>
 	<tr>
 		<th>Pag-Ibig Loan:</th>
-		<td><input type='text' class='numeric' id='PagibigLoan' name='PagibigLoan' value='<?php echo $PagibigLoan;?>'></td>
+		<td><input type='text' class='numeric' id='PagibigLoan' name='PagibigLoan' value='<?php echo $PagibigLoan;?>'>
+		</td><td style='text-align:left'><span class='warning' name='pagibig_loan' id='pagibig_loan'></span>
+		</td>
 	</tr>
 	<tr>
 		<th>SSS Loan:</th>
-		<td><input type='text' class='numeric' id='SSSLoan' name='SSSLoan' value='<?php echo $SSSLoan;?>'></td>
+		<td><input type='text' class='numeric' id='SSSLoan' name='SSSLoan' value='<?php echo $SSSLoan;?>'>
+		</td><td style='text-align:left'><span class='warning' name='sss_loan' id='sss_loan'></span>
+		</td>
 	</tr>
 	<tr>
 		<th>Company Loan:</th>
-		<td><input type='text' class='numeric' id='CompanyLoan' name='CompanyLoan' value='<?php echo $CompanyLoan;?>'></td>
+		<td><input type='text' class='numeric' id='CompanyLoan' name='CompanyLoan' value='<?php echo $CompanyLoan;?>'>
+		</td><td style='text-align:left'><span class='warning' name='comp_loan' id='comp_loan'></span>
+		</td>
 	</tr>
 	<tr>
 		<th>Cellphone Charges:</th>
-		<td><input type='text' class='numeric' id='CellphoneCharges' name='CellphoneCharges' value='<?php echo $CellphoneCharges;?>'></td>
+		<td><input type='text' class='numeric' id='CellphoneCharges' name='CellphoneCharges' value='<?php echo $CellphoneCharges;?>'>
+		</td><td style='text-align:left'><span class='warning' name='cellphone' id='cellphone'></span>
+		</td>
 	</tr>
 	<tr>
 		<th>Advances to Employee:</th>
-		<td><input type='text' class='numeric' id='AdvancestoEmployee' name='AdvancestoEmployee' value='<?php echo $AdvancestoEmployee;?>'></td>
+		<td><input type='text' class='numeric' id='AdvancestoEmployee' name='AdvancestoEmployee' value='<?php echo $AdvancestoEmployee;?>'>
+		</td><td style='text-align:left'><span class='warning' name='advances' id='advances'></span>
+		</td>
 	</tr>
 	<tr>
 		<th>Net Pay:</th>
@@ -170,7 +297,9 @@
 	</tr>
 	<tr>
 		<th>Status:</th>
-		<td><input type='text' id='Status' name='Status' value='<?php echo $Status;?>'></td>
+		<td><input type='text' id='Status' name='Status' value='<?php echo $Status;?>'>
+		</td><td style='text-align:left'><span class='warning' name='stat' id='stat'></span>
+		</td>
 	</tr>
 </table>
 
