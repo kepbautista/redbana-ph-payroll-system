1ad0c68e21d72a44c9ac0aa75d52f89ebbf4f970
abraham.darius.llave <abraham.darius.llave@65eff120-3760-e550-e617-1429e56cfc85>
2011-06-15 10:52:31 +0000

    something wrong with the redbana_payroll.sql file, copied the earlier lines from an earlier revision for it to be imported ih PHPmyAdmin. generated some random, session encryption key in config.php

--- payroll/redbana_payroll.sql
+++ payroll/redbana_payroll.sql
@@ -1,14 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 3.2.0.1
+-- version 3.3.9
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 10, 2011 at 12:07 PM
--- Server version: 5.1.36
---wa
--- PHP Version: 5.3.0
+-- Generation Time: May 24, 2011 at 04:22 PM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `redbana_payroll`
@@ -412,8 +417,10 @@ CREATE TABLE IF NOT EXISTS `payperiod` (
   `PAYROLL_FINALIZED` tinyint(1) NOT NULL DEFAULT '0',
   `PAYROLL_FINALIZED_BY` varchar(255) DEFAULT NULL,
   `PAYROLL_FINALIZED_DATE` timestamp NULL DEFAULT NULL,
+  `NEEDS_REGENERATION` tinyint(1) NOT NULL DEFAULT 0,
+  `NEEDS_REGENERATION_REASON` varchar(255) NOT NULL DEFAULT 'unspecified',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `payperiod`
