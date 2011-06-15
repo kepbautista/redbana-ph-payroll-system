6526489b53e502b13a83383c77d409de539d48f8
abraham.darius.llave <abraham.darius.llave@65eff120-3760-e550-e617-1429e56cfc85>
2011-06-15 10:52:31 +0000

    something wrong with the redbana_payroll.sql file, copied the earlier lines from an earlier revision for it to be imported ih PHPmyAdmin. generated some random, session encryption key in config.php

--- trunk/payroll/application/config/config.php
+++ trunk/payroll/application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = 'mysession';
+$config['encryption_key'] = '90124081iahdiabcmzzp324p4';
 
 /*
 |--------------------------------------------------------------------------
